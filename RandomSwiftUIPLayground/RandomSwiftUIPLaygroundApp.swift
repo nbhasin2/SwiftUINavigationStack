4c3a91211915b21f4657f3b40adc8a5587e7c0ec
Nishant Bhasin <nish.bhasin@gmail.com>
2023-04-02 09:10:33 -0400

    Initial Commit

---
+++ RandomSwiftUIPLayground/RandomSwiftUIPLaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomSwiftUIPLaygroundApp.swift
+//  RandomSwiftUIPLayground
+//
+//  Created by Nishant Bhasin on 2023-04-02.
+//
+
+import SwiftUI
+
+@main
+struct RandomSwiftUIPLaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
